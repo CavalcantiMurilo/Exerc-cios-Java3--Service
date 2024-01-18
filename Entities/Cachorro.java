099a528fdbc3d888244092f5f89aecfa2740b43a
Murilo Cavalcanti <106565777+CavalcantiMurilo@users.noreply.github.com>
2024-01-18 11:43:16 -0300

    Add files via upload

---
+++ Entities/Cachorro.java
@@ -0,0 +1,32 @@
+package Entities;
+
+public class Cachorro {
+
+    private String nome;
+    private String raca;
+    private int idade;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getRaca(){
+        return raca;
+    }
+
+    public void setRaca(String raca){
+        this.raca = raca;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
